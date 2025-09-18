43b073e9bfe1e5d28e95da71211ba71b15db82b2
Hlaloso8 <st10474466@rcconnect.edu.za>
2025-09-18 10:09:32 +0200

    initial commit

---
+++ test/loginapp/LoginTest.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package loginapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Hlaloso
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+
+    @Test
+    public void testCheckUsername() {
+    }
+
+    @Test
+    public void testCheckPasswordComplexity() {
+    }
+
+    @Test
+    public void testCheckCellPhoneNumber() {
+    }
+
+    @Test
+    public void testRegisterUser() {
+    }
+
+    @Test
+    public void testLoginUser() {
+    }
+
+    @Test
+    public void testReturnLoginStatus() {
+    }
+    
+}
---
+++ test/loginchat/LoginChatTest.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package loginchat;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Hlaloso
+ */
+public class LoginChatTest {
+    
+    public LoginChatTest() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+
+    @Test
+    public void testCheckUsername() {
+    }
+
+    @Test
+    public void testCheckPasswordComplexity() {
+    }
+
+    @Test
+    public void testCheckCellPhoneNumber() {
+    }
+
+    @Test
+    public void testRegisterUser() {
+    }
+
+    @Test
+    public void testLoginUser() {
+    }
+
+    @Test
+    public void testReturnLoginStatus() {
+    }
+    
+}

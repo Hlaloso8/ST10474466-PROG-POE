1a6ff44d7db4cbfff589727600271fe82ba65d02
Hlaloso8 <st10474466@rcconnect.edu.za>
2025-09-18 13:40:26 +0200

    adding changes

--- src/loginchat/PasswordComplexity.java
+++ src/loginchat/PasswordComplexity.java
@@ -4,10 +4,18 @@
  */
 package loginchat;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Hlaloso
  */
 public class PasswordComplexity {
+    public boolean checkPasswordComplexity(String password) {
+        if (password.length() < 8) return false;
+        if (!Pattern.compile("[A-Z]").matcher(password).find()) return false;
+        if (!Pattern.compile("[0-9]").matcher(password).find()) return false;
+        return Pattern.compile("[^A-Za-z0-9]").matcher(password).find();
+    }
     
 }

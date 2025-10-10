efcaa15d4f78e169b9f70e8a4399d07e4d053e6a
Hlaloso8 <st10474466@rcconnect.edu.za>
2025-10-10 14:34:12 +0200

    adding changes

---
+++ test/loginchat/MessageTest.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package loginchat;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Hlaloso
+ */
+public class MessageTest {
+    
+    public MessageTest() {
+    }
+
+    @Test
+    public void testCheckMessage() {
+    }
+
+    @Test
+    public void testCheckRecipient() {
+    }
+
+    @Test
+    public void testCheckMessageHash() {
+    }
+
+    @Test
+    public void testSetMessage() {
+    }
+
+    @Test
+    public void testPrintMessage() {
+    }
+
+    @Test
+    public void testReturnTotalMessages() {
+    }
+
+    @Test
+    public void testStoreMessage() {
+    }
+
+    @Test
+    public void testGetMessageID() {
+    }
+
+    @Test
+    public void testGetRecipient() {
+    }
+
+    @Test
+    public void testGetMessageText() {
+    }
+
+    @Test
+    public void testGetMessageHash() {
+    }
+
+    @Test
+    public void testIsSent() {
+    }
+
+    @Test
+    public void testSetRecipient() {
+    }
+
+    @Test
+    public void testSetMessageText() {
+    }
+
+    @Test
+    public void testGetTotalMessagesSent() {
+    }
+
+    @Test
+    public void testClearAllMessages() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+    
+}

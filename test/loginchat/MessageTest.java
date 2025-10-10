d49914d05b1667a4a90f1b16d0eea8e5c82cf188
Hlaloso8 <st10474466@rcconnect.edu.za>
2025-10-10 14:47:09 +0200

    adding changes

--- test/loginchat/MessageTest.java
+++ test/loginchat/MessageTest.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
+
 package loginchat;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author Hlaloso
- */
+
 public class MessageTest {
     
     public MessageTest() {
